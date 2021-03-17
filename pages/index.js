b2662a10a369caa2a773d8e1c2e1d932f9154e65
Peter <peter@ourstudio.se>
2021-03-17 11:29:35 +0100

    fixes initial setup of wayke

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
-import React, { useCallback } from 'react';
-import { WaykeProvider, WaykeSearch } from '@wayke-se/components-react'
+import React, { useCallback } from "react";
+import dynamic from "next/dynamic";
+
+const WaykeProvider = dynamic(
+  () => import("@wayke-se/components-react").then((mod) => mod.WaykeProvider),
+  {
+    ssr: false,
+    loading: () => <p>...</p>,
+  }
+);
+
+const WaykeSearch = dynamic(
+  () => import("@wayke-se/components-react").then((mod) => mod.WaykeSearch),
+  {
+    ssr: false,
+    loading: () => <p>...</p>,
+  }
+);
+
+const ProviderSettings = {
+  url: "https://test-ext-api.wayketech.se/vehicles",
+  useQueryParamsFromUrl: true,
+};
 
 function Index() {
   const onClickSearchItem = useCallback((id) => {
@@ -7,10 +28,13 @@ function Index() {
   }, []);
 
   return (
-    <WaykeProvider {...ProviderSettings}>
-      <WaykeSearch onClickSearchItem={onClickSearchItem} />
-    </WaykeProvider>
+    <>
+      <p>hello</p>
+      <WaykeProvider {...ProviderSettings}>
+        <WaykeSearch onClickSearchItem={onClickSearchItem} />
+      </WaykeProvider>
+    </>
   );
-};
+}
 
 export default Index;
